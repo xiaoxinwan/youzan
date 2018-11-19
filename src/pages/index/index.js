70ffbba96a1054133bb1d71bd34ddbd7b3a4cd41
xiaoxinwan <lixinxi1010@gmail.com>
2018-11-19 20:29:45 +0800

    触底加载数据

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -5,19 +5,43 @@ import Vue from 'vue'
 import axios from 'axios'
 import url from 'js/api.js'
 
+import { InfiniteScroll } from 'mint-ui'
+Vue.use(InfiniteScroll);
 
 new Vue({
     el: '#app',
-    data: { 
+    data: {
         lists: null,
-
+        pageNum: 1,
+        pageSize: 6,
+        loading: false, //true时不能加载
+        allLoaded: false
     },
-    created(){
-        axios.post(url.hotLists, {
-            pageNum: 1,
-            pageSize: 6
-        }).then(res => {
-            this.lists = res.data.lists
-        })
+    created() {
+        this.getLists()
+    },
+    methods: {
+        getLists() {
+            if(this.allLoaded) return
+            this.loading = true
+            axios.post(url.hotLists, {
+                pageNum: this.pageNum,
+                pageSize: this.pageSize
+            }).then(res => {
+                let curLists = res.data.lists
+                // 判断所有数据是否加载完毕
+                if(curLists.length < this.pageSize){
+                    this.allLoaded = true
+                }
+                if(this.lists){
+                    this.lists = this.lists.concat(curLists)
+                }else{
+                    // 第一次请求数据
+                    this.lists = curLists
+                }
+                this.loading = false
+                this.pageNum++
+            })
+        }
     }
 })
