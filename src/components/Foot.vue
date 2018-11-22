80b9d79ea7477f5a9cb95c307e78c441b4f94544
xiaoxinwan <lixinxi1010@gmail.com>
2018-11-22 22:45:05 +0800

    修复首页被遮，页面切换和焦点状态切换

--- src/components/Foot.vue
+++ src/components/Foot.vue
@@ -1,35 +1,54 @@
 <template>
-    <div class="bottom-nav">
-        <ul>
-            <li class="active">
-                <a href="index.html">
-                    <i class="icon-home"></i>
-                    <div>有赞</div>
-                </a>
-            </li>
-            <li>
-                <a href="category.html">
-                    <i class="icon-category"></i>
-                    <div>分类</div>
-                </a>
-            </li>
-            <li>
-                <a href="https://h5.youzan.com/v2/trade/cart?f_platform=yzapp&amp;source=yzapp">
-                    <i class="icon-cart"></i>
-                    <div>购物车</div>
-                </a>
-            </li>
-            <li>
-                <a href="https://h5.youzan.com/v2/buyer/member">
-                    <i class="icon-user"></i>
-                    <div>我</div>
-                </a>
-            </li>
-        </ul>
-    </div>
+  <div class="bottom-nav">
+    <ul>
+      <li :class="{active: index===curIndex}" v-for="(list, index) in navConfig" @click="changeNav(list,index)">
+        <a>
+          <i :class="list.icon"></i>
+          <div>{{list.name}}</div>
+        </a>
+      </li>
+    </ul>
+  </div>
 </template>
 <script>
-export default {};
+let navConfig = [
+  {
+    name: "有赞",
+    icon: "icon-home",
+    href: "index.html"
+  },
+  {
+    name: "分类",
+    icon: "icon-category",
+    href: "category.html"
+  },
+  {
+    name: "购物车",
+    icon: "icon-cart",
+    href: "cart.html"
+  },
+  {
+    name: "我",
+    icon: "icon-user",
+    href: "member.html"
+  }
+];
+
+import qs from "qs";
+
+export default {
+  data() {
+    return {
+      navConfig,
+      curIndex: parseInt(qs.parse(location.search.substr(1)).index) || 0
+    };
+  },
+  methods: {
+    changeNav(list, index) {
+      location.href = `${list.href}?index=${index}`;
+    }
+  }
+};
 </script>
 <style>
 .bottom-nav {
@@ -38,7 +57,7 @@ export default {};
   height: 50px;
   bottom: 0;
   background-color: #fff;
-  z-index: 8;
+  z-index: 9999;
   -o-border-image: url(https://b.yzcdn.cn/v2/image/wap/border-line-2.png) 2
     stretch;
   border-image: url(https://b.yzcdn.cn/v2/image/wap/border-line-2.png) 2 stretch;
